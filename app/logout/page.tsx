a5b7c2105bc5e5d9f0e214b310b3630407b2ee4d
sakib-xrz <sakibxrz21@gmail.com>
2025-03-17 14:06:06 +0600

    Implement logout functionality with token removal and middleware for route protection

---
+++ app/logout/page.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+"use client";
+
+import removeToken from "@/actions/removeToken";
+import { logout } from "@/redux/features/auth/authSlice";
+import { Loader2 } from "lucide-react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+export default function Logout() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const handleLogout = async () => {
+      dispatch(logout());
+      await removeToken();
+      window.location.href = "/";
+    };
+
+    handleLogout();
+  }, []);
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Loader2 size={32} className="animate-spin text-primary" />
+    </div>
+  );
+}
