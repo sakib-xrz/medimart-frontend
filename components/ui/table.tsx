91a9fdf0215985f6f7c506a132fae670748f1db2
sakib-xrz <sakibxrz21@gmail.com>
2025-03-23 00:04:32 +0600

    Enhance TableHead component with rounded top corners for improved styling

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -73,7 +73,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-10 bg-white px-2 text-left align-middle font-medium text-muted-foreground hover:bg-white [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+      "h-10 rounded-t-md bg-white px-2 text-left align-middle font-medium text-muted-foreground hover:bg-white [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
       className,
     )}
     {...props}
